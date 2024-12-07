a683b70a9cd80c68b9e0a8eac3fe878b458b55e8
Bob MacCallum <uncoolbob@gmail.com>
2024-12-07 11:43:56 +0000

    improve inspect() output

--- R/Entity-inspect.R
+++ R/Entity-inspect.R
@@ -27,17 +27,21 @@ setMethod("inspect", "Entity", function(object) {
   }
   
   ids_metadata <- metadata %>% filter(data_type == 'id')
-  variables_metadata <- metadata %>% filter(data_type != 'id')
+  variables_metadata <- metadata %>%
+    filter(data_type != 'id') %>%
+    select(-starts_with('entity_')) %>%
+    arrange(display_order)
   
   cat("TBC: entity-level metadata summarised here.\n")
   
   cat("\nID columns:\n")
-  print(ids_metadata %>% select(variable))
+  print(ids_metadata %>% select(variable, entity_name, entity_level))
   
   cat("\nVariable columns:\n")
   print(variables_metadata)
   
   cat("\nskim() summary of variable data:\n")
   
-  data %>% select(-all_of(ids_metadata$variable)) %>% skim()
+  skim_data <- data %>% select(-all_of(ids_metadata$variable)) %>% skim()
+  print(skim_data, include_summary = FALSE)
 })
