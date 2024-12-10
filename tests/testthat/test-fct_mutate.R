78a2cdf2a02705b182e76005b8716f98f0f30c85
Bob <uncoolbob@gmail.com>
2024-12-10 18:56:19 +0000

    added essential fct_mutate tests

--- tests/testthat/test-fct_mutate.R
+++ tests/testthat/test-fct_mutate.R
@@ -12,7 +12,6 @@ pets <- tibble(
 )
 
 test_that("fac_mutate() handles simple two-argument cases", {
-
   expect_silent(
     expect_equal(
       as.character(fct_mutate(dog, 'cat')),
@@ -27,15 +26,63 @@ test_that("fac_mutate() handles simple two-argument cases", {
       "cat"
     )
   )
-
   expect_silent(
     expect_equal(
       as.character(fct_mutate(factor=dog, true_value='cat')),
       "cat"
     )
   )
-
 })
 
-# test_that("fac_mutate() handles preserves levels and errors on non-level values", {
+
+test_that("fac_mutate() handles preserves levels", {
+  expect_equal(
+    levels(fct_mutate(dog, "puppy")),
+    pet_types
+  )
+})
   
+test_that("fac_mutate() errors on non-level values", {
+  expect_error(
+    oops <- fct_mutate(dog, "llama"),
+    "Error: Value 'llama' is not an existing level in the factor"
+  )
+})
+
+test_that("fac_mutate() conditions work on column mutations", {
+  expect_equal(
+    pets %>% mutate(
+      pet_type = fct_mutate(
+        pet_type,
+        pet_type == 'dog' & age <= 2,
+        'puppy'
+      )) %>% pull(pet_type) %>% as.character(),
+    c("puppy", "dog", "cat", "puppy", "cat")
+  )
+  
+  # also mutate for the negative condition
+  # and add a new level automatically
+  expect_equal(
+    pets %>% mutate(
+      pet_type = fct_mutate(
+        pet_type,
+        pet_type == 'dog' & age <= 2,
+        'puppy',
+        'not a puppy',
+        .auto_expand = TRUE
+      )) %>% pull(pet_type) %>% as.character(),
+    c("puppy", "not a puppy", "not a puppy", "puppy", "not a puppy")
+  )
+  
+  # for fun, let's try that another way
+  expect_equal(
+    pets %>% mutate(
+      pet_type = fct_mutate(
+        fct_recode(pet_type, 'not a puppy' = 'dog', 'not a puppy' = 'cat'),
+        pet_type == 'dog' & age <= 2,
+        'puppy',
+        'not a puppy'
+      )) %>% pull(pet_type) %>% as.character(),
+    c("puppy", "not a puppy", "not a puppy", "puppy", "not a puppy")
+  )
+})
