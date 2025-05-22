756d698cd0c25a4180f7b64424bb836851ed76a8
Bob <uncoolbob@gmail.com>
2025-05-22 21:48:58 +0100

    add entity validation

--- tests/testthat/test-Entity-collections.R
+++ tests/testthat/test-Entity-collections.R
@@ -8,15 +8,17 @@ test_that("create_variable_collection works", {
   participants <- study %>% get_entity('participant') %>% verbose()
   observations <- study %>% get_entity('observation') %>% verbose()
 
+  integer.measures.category_spec <- list(
+    category_name = "integer.measures",
+    children = c("Height..cm.", "Weight..kg."),
+    display_name = "integer-based anatomical measures",
+    definition = "integer-based anatomical measures"
+  )
+  
   expect_message(
     expect_message(
       observations <- observations %>%
-        create_variable_category(
-          category_name = "integer.measures",
-          children = c("Height..cm.", "Weight..kg."),
-          display_name = "integer-based anatomical measures",
-          definition = "integer-based anatomical measures"
-        ),
+        create_variable_category(!!!integer.measures.category_spec),
       "Successfully created category 'integer.measures'"
     ),
     "Made metadata update.s. to 'display_name', 'definition' for 'integer.measures'"
@@ -26,12 +28,6 @@ test_that("create_variable_collection works", {
   expect_true(
     observations %>% quiet() %>% validate()
   )
-
-  expect_no_error(
-    study <- study_from_entities(list(households, participants, observations), name = study_name)
-  )
-
-  observations <- study %>% get_entity('observation') %>% verbose()
 
   collection_spec = list(
     category = "integer.measures",
@@ -74,11 +70,36 @@ test_that("create_variable_collection works", {
     observations2 <- observations %>% create_variable_collection(!!!no_label_spec)
   )
 
+  # let's delete the variable category from observations2 and check
+  # that the entity no longer validates (orphan collection)
+  expect_message(
+    observations2 <- observations2 %>% delete_variable_category("integer.measures"),
+    "Category 'integer.measures' has been deleted"
+  )
+  # and category with a different name
+  desperate.measures_category_spec <- list_assign(integer.measures.category_spec, category_name = "desperate.measures")
+  expect_message(
+    expect_message(
+      observations2 <- observations2 %>% create_variable_category(!!!desperate.measures_category_spec)
+    )
+  )
+  
+  expect_warning(
+    observations2 %>% validate(),
+    "These variable collections have no corresponding variable category: integer.measures"
+  )
+  
   # check that a missing 'update' fails
   no_member_spec <- list_modify(collection_spec, member = zap())
   expect_error(
     observations2 <- observations %>% create_variable_collection(!!!no_member_spec),
     "missing field.+member"
   )
+
+  # TO DO: make study with entities with collections?  Only needed if we display
+  # collections in the inspect(study) overview
+#  expect_no_error(
+#    study <- study_from_entities(list(households, participants, observations), name = study_name)
+#  )
   
 })
