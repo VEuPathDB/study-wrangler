24b10701b62bac20fe20a412372d64a1a6930a3a
Bob <uncoolbob@gmail.com>
2025-11-13 23:59:39 +0000

    remove from .onLoad - use lazy evaluation of skim_with

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,32 @@
+#' skimr shortens all factor names to three characters by default.
+#' Create a custom skimmer that doesn't do this and trims top counts to 5.
+#' @export
+skim <- local({
+  .skim_custom <- NULL
+  function(...) {
+    if (is.null(.skim_custom)) {
+      .skim_custom <<- skimr::skim_with(
+        factor = skimr::sfl(
+          top_counts = function(x) {
+            tab <- table(x)
+            top <- sort(tab, decreasing = TRUE)
+            # Select the top 5 levels
+            top5 <- head(top, 5)
+            # Summarize the rest
+            if (length(top) > 5) {
+              others <- sum(top[-(1:5)])
+              top5 <- c(top5, `<Others>` = others)
+            }
+            # Format output
+            paste0(names(top5), ": ", top5, collapse = ", ")
+          }
+        )
+      )
+    }
+    .skim_custom(...)
+  }
+})
+
 # Convert R column types to `data_type` metadata annotation
 infer_data_type <- function(column, 
                             .allowed_data_types = NULL, 
