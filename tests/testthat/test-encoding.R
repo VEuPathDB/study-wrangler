b1f8ed6185b2fa9d1ee74b94c7e0cccfac3cb715
Bob <uncoolbob@gmail.com>
2026-04-28 09:41:40 +0100

    test detection function properly

--- tests/testthat/test-encoding.R
+++ tests/testthat/test-encoding.R
@@ -1,3 +1,18 @@
+test_that("detect_file_encoding identifies UTF-8", {
+  file_path <- system.file("extdata", "toy_example/households.tsv", package = "study.wrangler")
+  expect_equal(study.wrangler:::detect_file_encoding(file_path), "UTF-8")
+})
+
+test_that("detect_file_encoding identifies ISO-8859-1", {
+  file_path <- system.file("extdata", "toy_example/householdsISO8859.tsv", package = "study.wrangler")
+  expect_equal(study.wrangler:::detect_file_encoding(file_path), "ISO-8859-1")
+})
+
+test_that("detect_file_encoding identifies Windows-1252", {
+  file_path <- system.file("extdata", "toy_example/householdsWindows1252.tsv", package = "study.wrangler")
+  expect_equal(study.wrangler:::detect_file_encoding(file_path), "Windows-1252")
+})
+
 test_that("entity_from_file handles ISO-8859-1 encoded files", {
   file_path <- system.file("extdata", "toy_example/householdsISO8859.tsv", package = 'study.wrangler')
 
